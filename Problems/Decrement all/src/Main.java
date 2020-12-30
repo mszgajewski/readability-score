66041b3a41f106b88b87c31bddc9f945291a5eed
Mariusz <56251640+mszgajewski@users.noreply.github.com>
2020-12-30 18:40:42 +0100

    Add files via upload

---
+++ Problems/Decrement all/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (scanner.hasNextInt()){
+            System.out.print(scanner.nextInt()--);
+        }
+    }
+}
